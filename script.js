111221b0b117084e105a879deaaeff1e89ae977d
Sf-Shohag71 <shakhfarid71@gmail.com>
2024-02-09 20:55:34 +0600

    Tracking each boxes

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 
+let turn0 = true; //PlayerX, player0
+
+const winPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    // console.log("button was clicked");
+    if (turn0) {
+      box.innerHTML = "0";
+      turn0 = false;
+    } else {
+      box.innerHTML = "X";
+      turn0 = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+    for(let pattern of winPattern){
+        console.log(pattern[0], pattern[1], pattern[2]);
+        console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]]);
+    }
+}
